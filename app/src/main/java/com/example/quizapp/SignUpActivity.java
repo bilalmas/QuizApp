df18b078006d23a44eb4fa988244d78cebed6231
suleman137 <suleman137d@gmail.com>
2020-06-21 23:43:25 +0500

    Sign Up UI made

---
+++ app/src/main/java/com/example/quizapp/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //startBtn = findViewById(R.id.start_btn);
+
+
+    }
+}
