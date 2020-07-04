4206aba95353e63f14e9bef536370b4e56de15f0
suleman137 <suleman137d@gmail.com>
2020-07-04 16:09:39 +0500

    Sign in and Sign Up added

--- app/src/main/java/com/example/quizapp/SignInActivity.java
+++ app/src/main/java/com/example/quizapp/SignInActivity.java
@@ -1,16 +1,28 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private TextView signuptextview;
+    EditText full_name, email_address, password, confirm_password;
+    Button SignIn;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
@@ -18,7 +30,59 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        signuptextview = findViewById(R.id.textViewSignUp2);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        email_address = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editTextPassword);
+        SignIn = findViewById(R.id.buttonCreateAccount);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(SignInActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(SignInActivity.this, CategoriesActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(SignInActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_address.getText().toString();
+                String pwd = password.getText().toString();
+
+                if(email.isEmpty()){
+                    email_address.setError("Please enter Email Address");
+                    email_address.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("Please enter your Password");
+                    password.requestFocus();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(SignInActivity.this,"SignIn Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(SignInActivity.this,CategoriesActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignInActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/quizapp/SignUpActivity.java
+++ app/src/main/java/com/example/quizapp/SignUpActivity.java
@@ -1,17 +1,27 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    //private Button startBtn;
-    private TextView signintextview;
+    EditText full_name, email_address, password, confirm_password;
+    Button SignUp;
+    FirebaseAuth mFirebaseAuth;
+
 
 
     @Override
@@ -19,9 +29,56 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //startBtn = findViewById(R.id.start_btn);
-        signintextview = findViewById(R.id.textViewSignin);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        full_name = findViewById(R.id.editTextName);
+        email_address = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editTextPassword);
+        confirm_password = findViewById(R.id.editTextConfirmPassword);
+        SignUp = findViewById(R.id.buttonCreateAccount);
 
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = full_name.getText().toString();
+                String email = email_address.getText().toString();
+                String pwd = password.getText().toString();
+                String confirm_pwd = confirm_password.getText().toString();
+
+                if(name.isEmpty()){
+                    full_name.setError("Please enter your Name");
+                    full_name.requestFocus();
+                }
+                else if(email.isEmpty()){
+                    email_address.setError("Please enter Email Address");
+                    email_address.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("Please enter your Password");
+                    password.requestFocus();
+                }
+                else  if(!confirm_pwd.equals(pwd)){
+                    confirm_password.setError("Passwords do not match");
+                    confirm_password.requestFocus();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
     public void Signin(View view){
